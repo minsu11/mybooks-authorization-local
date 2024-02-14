78699be3580b8a2a48e0f3d9742fe4d3c850938b
masiljangajji <hjk172262@gmail.com>
2024-02-14 21:14:42 +0900

    test: jen test

--- src/main/java/store/mybooks/authorization/AuthorizationApplication.java
+++ src/main/java/store/mybooks/authorization/AuthorizationApplication.java
@@ -8,7 +8,7 @@ public class AuthorizationApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("For Jenkins Test8");
+		System.out.println("For Jenkins Test9");
 		SpringApplication.run(AuthorizationApplication.class, args);
 	}
 
