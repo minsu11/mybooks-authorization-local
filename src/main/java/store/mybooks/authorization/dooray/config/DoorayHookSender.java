45c48fe466fbfe55eb9cb5e259ea84b8c964cb82
masiljangajji <hjk172262@gmail.com>
2024-02-26 17:09:21 +0900

    docs: JavaDoc 추가

--- src/main/java/store/mybooks/authorization/dooray/config/DoorayHookSender.java
+++ src/main/java/store/mybooks/authorization/dooray/config/DoorayHookSender.java
@@ -7,6 +7,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 
+/**
+ * packageName    : store.mybooks.authorization.dooray.config<br>
+ * fileName       : DoorayHookSender<br>
+ * author         : masiljangajji<br>
+ * date           : 2/25/24<br>
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/25/24        masiljangajji       최초 생성
+ */
 @Component
 @Getter
 @Setter
--- src/main/java/store/mybooks/authorization/dooray/config/MessageSender.java
+++ src/main/java/store/mybooks/authorization/dooray/config/MessageSender.java
@@ -1,6 +1,17 @@
 package store.mybooks.authorization.dooray.config;
 
 
+/**
+ * packageName    : store.mybooks.authorization.dooray.config<br>
+ * fileName       : MessageSender<br>
+ * author         : masiljangajji<br>
+ * date           : 2/25/24<br>
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/25/24        masiljangajji       최초 생성
+ */
 public interface MessageSender {
 
     boolean sendMessage(MessageBot messageBot, String message);
