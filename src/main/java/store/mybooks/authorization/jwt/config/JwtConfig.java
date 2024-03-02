e52628f491323e73f0237704c873fa47ce762efb
masiljangajji <hjk172262@gmail.com>
2024-03-02 15:46:09 +0900

    refactor: JwtConfig 변경

--- src/main/java/store/mybooks/authorization/jwt/config/JwtConfig.java
+++ src/main/java/store/mybooks/authorization/jwt/config/JwtConfig.java
@@ -1,8 +1,10 @@
 package store.mybooks.authorization.jwt.config;
 
 public interface JwtConfig {
-	String SECRET = "이승재"; // 우리 서버만 알고 있는 비밀값
-	int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
-//	String TOKEN_PREFIX = "Bearer ";
-//	String HEADER_STRING = "Authorization";
+
+    // todo 여기있는 것들 전부다 키체인에 넣어서 관리하기
+    String SECRET = "이승재"; // 우리 서버만 알고 있는 비밀값
+    String ISSUER = "my-books";
+    int EXPIRATION_TIME = 1800000; // 30분 (1/1000초)
+
 }
