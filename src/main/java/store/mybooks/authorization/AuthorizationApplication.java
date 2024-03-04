471051001d401dcfc8039713232da31883065796
masiljangajji <hjk172262@gmail.com>
2024-03-04 17:13:55 +0900

    refactor: AuthorizationApplication @ConfigurationPropertiesScan 추가

--- src/main/java/store/mybooks/authorization/AuthorizationApplication.java
+++ src/main/java/store/mybooks/authorization/AuthorizationApplication.java
@@ -2,12 +2,15 @@ package store.mybooks.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AuthorizationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthorizationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
 
 }
