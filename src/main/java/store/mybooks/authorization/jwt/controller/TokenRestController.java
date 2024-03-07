8c41c6c6e797bb7943186573b5513cf415f488a8
masiljangajji <hjk172262@gmail.com>
2024-03-08 01:38:12 +0900

    refactor: TokenRestController createToken 로직변경 (리프래시토큰을 따로 발급받지 않음 , 레디스에 리프래시토큰 역할을 하는 비밀키를 넣음)

--- src/main/java/store/mybooks/authorization/jwt/controller/TokenRestController.java
+++ src/main/java/store/mybooks/authorization/jwt/controller/TokenRestController.java
@@ -1,6 +1,8 @@
 package store.mybooks.authorization.jwt.controller;
 
+import java.time.Duration;
 import java.util.Enumeration;
+import java.util.Objects;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.mybooks.authorization.config.JwtConfig;
+import store.mybooks.authorization.config.KeyConfig;
+import store.mybooks.authorization.config.RedisConfig;
+import store.mybooks.authorization.jwt.dto.request.RefreshTokenRequest;
 import store.mybooks.authorization.jwt.dto.request.TokenRequest;
+import store.mybooks.authorization.jwt.dto.response.RefreshTokenResponse;
 import store.mybooks.authorization.jwt.dto.response.TokenResponse;
 import store.mybooks.authorization.jwt.service.TokenService;
+import store.mybooks.authorization.redis.RedisService;
 
 /**
  * packageName    : store.mybooks.authorization.auth.controller<br>
@@ -33,15 +41,36 @@ public class TokenRestController {
 
     private final TokenService authService;
 
+    private final RedisService redisService;
+
+    private final JwtConfig jwtConfig;
+
+    private final RedisConfig redisConfig;
+
+    private final KeyConfig keyConfig;
+
+
     @PostMapping
     public ResponseEntity<TokenResponse> createToken(
-            @RequestBody TokenRequest tokenRequest) {
+            @RequestBody TokenRequest tokenRequest, HttpServletRequest request) {
 
         String accessToken = authService.createAccessToken(tokenRequest);
-        String refreshToken = authService.createRefreshToken(accessToken);
 
-        return new ResponseEntity<>(new TokenResponse(accessToken, refreshToken), HttpStatus.CREATED);
+        System.out.println("!!!!");
+        System.out.println(request.getRemoteAddr());
+        //  Key {ip주소} - Value {리프래시토큰}
+
+        // 키메니저에서 설정한 비밀 값 + 엑세스 토큰 + 유저 ip 주소
+        // 엑세스 토큰은 ip주소가 우연히 같을 수 있기 떄문에 넣어줌
+
+        redisService.setValues(keyConfig.keyStore(redisConfig.getRedisKey()) + accessToken + request.getRemoteAddr(),
+                keyConfig.keyStore(redisConfig.getRedisValue()),
+                Duration.ofMillis(jwtConfig.getRefreshExpiration()));
+
+        return new ResponseEntity<>(new TokenResponse(accessToken), HttpStatus.CREATED);
     }
+
+
 
 
 }
