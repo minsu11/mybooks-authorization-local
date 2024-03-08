5e595d0bbb2c761bfefa0bffeafcf3c2e657e292
masiljangajji <hjk172262@gmail.com>
2024-03-08 17:23:12 +0900

    refactor: TokenRestController refreshAccessToken 리프래시토큰 검증로직 변경 , @RequestBody 추가

--- src/main/java/store/mybooks/authorization/jwt/controller/TokenRestController.java
+++ src/main/java/store/mybooks/authorization/jwt/controller/TokenRestController.java
@@ -64,6 +64,7 @@ public class TokenRestController {
         System.out.println("!!!!");
         System.out.println(request.getRemoteAddr());
         System.out.println(accessToken + request.getRemoteAddr() + " 내가 넣을 키 값");
+        System.out.println(accessToken + "   이게 내가 줄 값임");
 
         // Key = 엑세스 토큰 + ip 주소
         // Value = (키메너지가 관리하는 암호 + ip주소) 를 BCrypt 로 잠근 값
@@ -75,14 +76,15 @@ public class TokenRestController {
         // 따라서 리프래시토큰에 대한 변조를 막을 수 있고 , 만료시간이 지나면 자동으로 사라지기 떄문에 관리가 가능 함
 
         redisService.setValues(accessToken + request.getRemoteAddr(),
-                passwordEncoder.encode(keyConfig.keyStore(redisConfig.getRedisValue() + request.getRemoteAddr())),
+                passwordEncoder.encode(keyConfig.keyStore(redisConfig.getRedisValue()) + request.getRemoteAddr()),
                 Duration.ofMillis(jwtConfig.getRefreshExpiration()));
+
 
         return new ResponseEntity<>(new TokenResponse(accessToken), HttpStatus.CREATED);
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<RefreshTokenResponse> refreshAccessToken(RefreshTokenRequest refreshTokenRequest,
+    public ResponseEntity<RefreshTokenResponse> refreshAccessToken(@RequestBody RefreshTokenRequest refreshTokenRequest,
                                                                    HttpServletRequest request) {
 
         String key = refreshTokenRequest.getAccessToken() + request.getRemoteAddr();
@@ -90,30 +92,42 @@ public class TokenRestController {
         String refreshToken = redisService.getValues(key);
 
         // 만료됐는지 , 유효한지 검증
-        if (Objects.isNull(refreshToken) || !passwordEncoder.matches(key,
-                refreshToken)) { // null 이면 만료된 것 , BCrypt 로 잠근 값을 매치로 확인해 , 내가 넣어준 유효한 리프래시 토큰인지 검증
+
+        System.out.println(refreshTokenRequest.getAccessToken()+" 내가 받은 거");
+        System.out.println(refreshToken+"이게 키 값");
+
+        // 키 = 엑세스토큰 + ip주소
+        // 벨류 = 암호화한 값 + ip주소
+
+        if (Objects.isNull(refreshToken) || !passwordEncoder.matches(keyConfig.keyStore(redisConfig.getRedisValue())+request.getRemoteAddr(), refreshToken)) { // null 이면 만료된 것 , BCrypt 로 잠근 값을 매치로 확인해 , 내가 넣어준 유효한 리프래시 토큰인지 검증
             return new ResponseEntity<>(new RefreshTokenResponse(false, null), HttpStatus.NO_CONTENT);
         }
 
         String newAccessToken = authService.refreshAccessToken(refreshTokenRequest); // 엑세스토큰 새로 만들고
         redisService.deleteValues(key); // 기존에 있던 리프래시 토큰을 삭제
 
+        System.out.println(newAccessToken);
+
         // 리프래시토큰 갱신
         redisService.setValues(newAccessToken + request.getRemoteAddr(),
-                passwordEncoder.encode(keyConfig.keyStore(redisConfig.getRedisValue() + request.getRemoteAddr())),
+                passwordEncoder.encode(keyConfig.keyStore(redisConfig.getRedisValue()) + request.getRemoteAddr()),
                 Duration.ofMillis(jwtConfig.getRefreshExpiration()));
+
+        System.out.println(redisService.getValues(newAccessToken+request.getRemoteAddr()));
 
         // 새로운 엑세스 토큰을 발행 함
         return new ResponseEntity<>(new RefreshTokenResponse(true, newAccessToken), HttpStatus.CREATED);
     }
 
+
+
+
     @DeleteMapping("/logout")
-    public ResponseEntity<Void> deleteRefreshToken(LogoutRequest logoutRequest,HttpServletRequest request){
+    public ResponseEntity<Void> deleteRefreshToken(@RequestBody LogoutRequest logoutRequest, HttpServletRequest request) {
 
         // 기존에 있는 토큰으로는 재발급 못받도록 , 리프래시 토큰 삭제
-        redisService.deleteValues(logoutRequest.getAccessToken()+request.getRemoteAddr());
+        redisService.deleteValues(logoutRequest.getAccessToken() + request.getRemoteAddr());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
 
 }
