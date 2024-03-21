fdd19ad4b55a729550880eb3138907aebf8cfcfe
masiljangajji <hjk172262@gmail.com>
2024-03-21 11:08:20 +0900

    refactor: request dto allArg 추가

--- src/main/java/store/mybooks/authorization/jwt/dto/request/RefreshTokenRequest.java
+++ src/main/java/store/mybooks/authorization/jwt/dto/request/RefreshTokenRequest.java
@@ -1,5 +1,6 @@
 package store.mybooks.authorization.jwt.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
  */
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenRequest {
 
     private String accessToken;
