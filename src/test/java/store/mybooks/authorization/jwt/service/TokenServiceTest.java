b56861ccc5939df311562d3c37e42ca9f49f22fc
masiljangajji <hjk172262@gmail.com>
2024-03-21 17:54:05 +0900

    test:TokenServiceTest 변경

--- src/test/java/store/mybooks/authorization/jwt/service/TokenServiceTest.java
+++ src/test/java/store/mybooks/authorization/jwt/service/TokenServiceTest.java
@@ -1,24 +1,20 @@
 package store.mybooks.authorization.jwt.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import java.util.Date;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import store.mybooks.authorization.config.JwtConfig;
 import store.mybooks.authorization.config.KeyConfig;
 import store.mybooks.authorization.jwt.dto.request.TokenRequest;
@@ -48,59 +44,66 @@ class TokenServiceTest {
     @Mock
     KeyConfig keyConfig;
 
-
+    
     @Test
     @DisplayName("TokenRequest 로 CreateAccessToken 실행시 동작 테스트")
     void givenTokenRequest_whenCallCreateAccessToken_thenReturnToken(@Mock TokenRequest request) {
 
+        String authority = "ROLE_ADMIN";
+        String status = "ACTIVE";
+        String uuid = "mockUuid";
 
         when(request.getIsAdmin()).thenReturn(true);
-        when(request.getStatus()).thenReturn("TEST");
-        when(request.getUuid()).thenReturn("testUUID");
+        when(request.getStatus()).thenReturn("ACTIVE");
+        when(request.getUuid()).thenReturn("mockUuid");
 
-        when(jwtConfig.getIssuer()).thenReturn("my-books");
-        when(jwtConfig.getSecret()).thenReturn("secret");
-        when(jwtConfig.getAccessExpiration()).thenReturn(1800000L);
-        when(keyConfig.keyStore(anyString())).thenReturn("key-secret");
+        when(jwtConfig.getIssuer()).thenReturn("mockIssuer");
+        when(jwtConfig.getAccessExpiration()).thenReturn(3600L);
+        when(jwtConfig.getSecret()).thenReturn("mockSecret");
+        when(keyConfig.keyStore("mockSecret")).thenReturn("mockKeyStore");
 
-        String token = tokenService.createAccessToken(request);
+        String accessToken = tokenService.createAccessToken(request);
+
+
+        DecodedJWT jwt = JWT.decode(accessToken);
+        assertEquals("mockIssuer", jwt.getIssuer());
+        assertEquals(uuid, jwt.getSubject());
+        assertEquals(authority, jwt.getClaim("authority").asString());
+        assertEquals(status, jwt.getClaim("status").asString());
 
         verify(request, times(1)).getStatus();
         verify(request, times(1)).getUuid();
         verify(request, times(1)).getIsAdmin();
-
-
-        Algorithm algorithm = Algorithm.HMAC512(keyConfig.keyStore(jwtConfig.getSecret()));
-        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
-
-        DecodedJWT jwt = jwtVerifier.verify(token);
-
-        assertEquals(token, jwt.getToken());
-        assertEquals("testUUID", jwt.getSubject());
-        assertEquals("my-books", jwt.getIssuer());
-        assertEquals("ROLE_ADMIN", jwt.getClaim("authority").asString());
     }
 
     @Test
     @DisplayName("기존 엑세스 토큰을 디코딩한 DecodedJwt 로 refreshAccessToken 실행시 동작 테스트")
-    void givenDecodedJwt_whenCallRefreshAccessToken_thenReturnToken(@Mock DecodedJWT jwt, @Mock Claim authClaim, @Mock Claim statusClaim) {
+    void givenDecodedJwt_whenCallRefreshAccessToken_thenReturnToken() {
 
+        String subject = "mockSubject";
+        String authority = "ROLE_USER";
+        String status = "ACTIVE";
+        Date issuedAt = new Date();
 
-        when(jwt.getClaim("authority")).thenReturn(authClaim);
-        when(jwt.getClaim("status")).thenReturn(statusClaim);
-        when(jwtConfig.getIssuer()).thenReturn("my-books");
-        when(jwtConfig.getSecret()).thenReturn("secret");
-        when(jwtConfig.getAccessExpiration()).thenReturn(1800000L);
-        when(keyConfig.keyStore(anyString())).thenReturn("key-secret");
+        String token = JWT.create()
+                .withSubject(subject)
+                .withClaim("authority", authority)
+                .withClaim("status", status)
+                .withIssuedAt(issuedAt)
+                .sign(Algorithm.HMAC512("mockSecret")); // Mocking secret key
 
-        tokenService.refreshAccessToken(jwt);
+        when(jwtConfig.getIssuer()).thenReturn("mockIssuer");
+        when(jwtConfig.getAccessExpiration()).thenReturn(3600L); // 1 hour expiration
+        when(jwtConfig.getSecret()).thenReturn("mockSecret");
+        when(keyConfig.keyStore("mockSecret")).thenReturn("mockKeyStore");
 
-        verify(jwtConfig, times(1)).getIssuer();
-        verify(jwtConfig, times(1)).getSecret();
-        verify(jwtConfig,times(1)).getAccessExpiration();
-        verify(jwt, times(1)).getClaim("authority");
-        verify(jwt, times(1)).getClaim("status");
+        String newAccessToken = tokenService.refreshAccessToken(JWT.decode(token));
 
+        DecodedJWT newJwt = JWT.decode(newAccessToken);
+        assertEquals(subject, newJwt.getSubject());
+        assertEquals(authority, newJwt.getClaim("authority").asString());
+        assertEquals(status, newJwt.getClaim("status").asString());
+        assertEquals("mockIssuer", newJwt.getIssuer());
 
     }
 }
